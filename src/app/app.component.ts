4767561918e37fc6b92cb562e3d6eb2be8a8ef40
ddeveloper72 <duncan.niamh@gmail.com>
2019-07-27 11:47:34 +0100

    Demonstration of using form.patchValue to update a specific form item as opposed to setValue for updating all form items.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,14 +17,19 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.signupForm.setValue({
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender:  'Male'
+    // });
+    this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName,
-        email: ''
-      },
-      secret: 'pet',
-      questionAnswer: '',
-      gender:  'Male'
+        username: suggestedName
+      }
     });
   }
 
