6c0b549bb2a1ce33b14592718d928965ea1219d0
ddeveloper72 <duncan.niamh@gmail.com>
2019-07-27 11:01:55 +0100

    Included 2-way data binding in the export class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f', { static: false })
   signupForm: NgForm;
+  answer = '';
 
   defaultQuestion = 'pet';
   defaultUsername = 'eg. Bob';
